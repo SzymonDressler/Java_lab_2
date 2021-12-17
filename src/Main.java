93007948ce07b4be4165e7d9a501b62af51b3f2d
Szymon <szymondressler@gmail.com>
2021-12-17 11:38:33 +0100

    Cwiczenie_4

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,40 @@ public class Main {
         c) w trzeciej pętli return
         */
 
+        System.out.print("break:\n");
+        for( int a=0;a<=9;a++){
+
+            if(a==7)
+
+                break;
+
+            System.out.println(a + " ");
+
+        }
+        System.out.print("continue:\n");
+
+        for (int b = 0;b<=162;b++){
+
+            if( b% 94== 0)
+
+                continue;
+
+            System.out.println(b + " ");
+
+        }
+        System.out.print("return:\n");
+
+        for (int c = 49;c>=0;c--){
+
+            if(c==42)
+
+                return;
+
+            System.out.println(c + " ");
+        }
 
     }
 }
+
+
+
