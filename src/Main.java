26d0810d47a29eaa6c0bb46544ec2849b01c1e77
Szymon <szymondressler@gmail.com>
2021-12-17 11:25:09 +0100

    Cwiczenie 2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,11 +10,29 @@ public class Main {
          *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
          * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
 
+        int lastDigitOfIndex = 2;
+        for (int i = 100; i >= 0; i--) {
+            if (i % lastDigitOfIndex == 0) {
+                System.out.println(i);
+            }
+        }
+
 
         /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
          * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
          * za pomocą pętli forEach powiększone o 11 */
+        int[] integerArray = new int[5];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            integerArray[i] = scanner.nextInt();
+        }
+
+        for (int number :
+                integerArray) {
+            System.out.println(number + 11);
+        }
+    }
 
 
     }
-}
+
